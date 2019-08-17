036bf5c60cfeece77ff5d5f89876ed477ae6197d
chowhr1231 <thddbswo14@gmail.com>
2019-08-17 18:21:23 +0900

    [MOD] 웹페이지 연결작업
    기존에는 connect 메서드로 해왔지만 객체를 생성하면서
    바로 웹페이지랑 연결하는 작업을 하도록 수정

--- src/main/java/crawling/Crawl.java
+++ src/main/java/crawling/Crawl.java
@@ -7,15 +7,10 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class Crawl {
-    private String url;
     private Document document;
 
-    public Crawl(String url) {
-        this.url = url;
-    }
-
-    public void connect() throws IOException {
-        document = Jsoup.connect(url).get();
+    public Crawl(String url) throws IOException {
+        this.document = Jsoup.connect(url).get();
     }
 
     public Elements extractCSS(String css) {
