69e7866fcdb841a795a819904fa4a6f008e98581
chowhr1231 <thddbswo14@gmail.com>
2019-08-18 15:55:23 +0900

    [MOD] 분기처리
    args가 두개인 경우 crawling할 url를 수집하는 작업
    args가 한개인 경우 crawling하는 작업

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,17 @@
 import crawling.Crawl;
+import crawling.ExcelVO;
 import crawling.UrlVO;
 import excel.WriteExcel;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-import javax.xml.bind.Element;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -20,49 +24,57 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws Exception {
         Main.setSSL();
-
-        String url = "https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=49&buyYearChk=2019&page=1";
         List<Map<String, String>> dataList = new ArrayList<>();
 
-        try {
-            Crawl urlCrawling = new Crawl(url);
-            String lastPage = urlCrawling.extractCSS("a.last").attr("href").replaceAll("[^0-9]", "");
+        if (args.length == 2) {
+            String url = args[0];
+            String fileName = args[1];
 
-            Elements data;
-            UrlVO urlVO;
-            Crawl crawl;
-            for (int page = 1; page <= Integer.parseInt(lastPage); page++) {
-                crawl = new Crawl("https://www.bobaedream.co.kr/mycar/mycar_list.php?gubun=K&maker_no=49&buyYearChk=2019&page=" + page);
-                data = crawl.extractCSS("a.img");
-                urlVO = new UrlVO();
-                dataList.addAll(urlVO.findNextUrls(data));
+            try {
+                Crawl urlCrawling = new Crawl(url);
+                String lastPage = urlCrawling.extractCSS("a.last").attr("href").replaceAll("[^0-9]", "");
+
+                Elements data;
+                UrlVO urlVO;
+                Crawl crawl;
+                for (int page = 1; page <= Integer.parseInt(lastPage); page++) {
+                    crawl = new Crawl(url.substring(0, url.length() - 1) + page);
+                    data = crawl.extractCSS("a.img");
+                    urlVO = new UrlVO();
+                    dataList.addAll(urlVO.findNextUrls(data));
+                }
+
+                WriteExcel work = new WriteExcel();
+                work.saveExcel(dataList, fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        } else if (args.length == 1) {
+            System.out.println("=================================");
+            String readFileName = args[0];
+            FileInputStream fis = new FileInputStream(readFileName);
+            HSSFWorkbook workbook = new HSSFWorkbook(fis);
+            HSSFSheet sheet = workbook.getSheetAt(0);
 
-            WriteExcel work = new WriteExcel();
-            work.saveExcel(dataList, "hyundai_2019.xls");
-        } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                for (int rowidx=1; rowidx < sheet.getPhysicalNumberOfRows(); rowidx++) {
+                    String url = sheet.getRow(rowidx).getCell(0).getStringCellValue();
+                    Crawl carPage = new Crawl(url);
+                    Elements titleElements = carPage.extractCSS("div.select-finder");
+                    Element mainElements = carPage.extractCSS("tbody").get(1);
+                    ExcelVO carDTO = new ExcelVO();
+                    carDTO.extractTitleData(titleElements);
+                    carDTO.extractMainData(mainElements.select("tr"));
+
+                    dataList.add(carDTO.getCarData());
+                }
+
+                WriteExcel work = new WriteExcel();
+                work.saveExcel(dataList, "car_data.xls");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
-
-//        try {
-//            for (String url : urlList) {
-//                Crawl carPage = new Crawl(url);
-//                carPage.connect();
-//                Elements titleElements = carPage.extractCSS("div.select-finder");
-//                Element mainElements = carPage.extractCSS("tbody").get(1);
-//                ExcelVO carDTO = new ExcelVO();
-//                carDTO.extractTitleData(titleElements);
-//                carDTO.extractMainData(mainElements.select("tr"));
-//
-//                dataList.add(carDTO.getCarData(), "car_data.xls);
-//            }
-//
-//            WriteExcel work = new WriteExcel();
-//            work.saveExcel(dataList);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 
     private static void setSSL() throws NoSuchAlgorithmException, KeyManagementException {
