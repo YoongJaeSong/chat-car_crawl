29467f9a13d907990d6d7291e36f6c64e304b57f
chowhr1231 <thddbswo14@gmail.com>
2019-08-18 17:53:28 +0900

    [ADD] 기존 Excel파일인 경우
    매번 파일을 새로 만드는 것이 아니라 파일의 존재 여부를
    확인후 있으면 update하는 방식을 추가
    
    파일에 쓰는 중복 작업 메서드로 리펙토링

--- src/main/java/excel/WriteExcel.java
+++ src/main/java/excel/WriteExcel.java
@@ -1,10 +1,10 @@
 package excel;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -12,12 +12,20 @@ import java.util.Map;
 import java.util.Set;
 
 public class WriteExcel {
-    public void saveExcel(List<Map<String, String>> carList, String fileName) {
+    private List<Map<String, String>> carList;
+    private Set<String> keys;
+    private String fileName;
+
+    public WriteExcel(List<Map<String, String>> carList, String fileName) {
+        this.carList = carList;
+        this.fileName = fileName;
+        keys = carList.get(0).keySet();
+    }
+
+    public void saveNewExcel() {
         HSSFWorkbook workbook = new HSSFWorkbook();
         HSSFSheet sheet = workbook.createSheet("car data");
         HSSFRow titleRow = sheet.createRow(0);
-        HSSFCell cell;
-        Set<String> keys = carList.get(0).keySet();
 
         int idx = 0;
         for (String key : keys) {
@@ -25,16 +33,7 @@ public class WriteExcel {
             idx++;
         }
 
-        int rowLine = 1;
-        for (Map<String, String> carData : carList) {
-            HSSFRow valueRow = sheet.createRow(rowLine);
-            idx = 0;
-            for (String key : keys) {
-                valueRow.createCell(idx).setCellValue(carData.get(key));
-                idx++;
-            }
-            rowLine++;
-        }
+        saveExcel(sheet, 1);
 
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(fileName);
@@ -44,5 +43,39 @@ public class WriteExcel {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void saveExistExcel() throws IOException {
+        HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(fileName));
+        HSSFSheet sheet = workbook.getSheetAt(0);
+        int lastRow = sheet.getPhysicalNumberOfRows();
+
+        saveExcel(sheet, lastRow);
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            workbook.write(fileOutputStream);
+            fileOutputStream.close();
+            System.out.println("엑셀 저장 성공");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private HSSFRow saveExcel(HSSFSheet sheet, int rowIdx) {
+        HSSFRow row = null;
+        int cellIdx;
+
+        for (Map<String, String> carData : carList) {
+            row = sheet.createRow(rowIdx);
+            cellIdx = 0;
+            for (String key : keys) {
+                row.createCell(cellIdx).setCellValue(carData.get(key));
+                cellIdx++;
+            }
+            rowIdx++;
+        }
+
+        return row;
     }
 }
